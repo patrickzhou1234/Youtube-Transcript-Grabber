ec8f70f8fc44cd948c4ced5e119e8ac5b55eaba2
Patrick Zhou <patrickchang44543@gmail.com>
2023-08-31 17:49:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 document.querySelectorAll(".yt-spec-touch-feedback-shape")[9].click();
 setTimeout(() => {
     document.querySelectorAll(".ytd-menu-service-item-renderer")[8].click();
-}, 500);
-var st="";
-setTimeout(() => {
-document.querySelectorAll(".segment-text").forEach((el) => {
-    st+=el.textContent;
-});
-copy(st);
 }, 1000);
+var st = "";
+with ({ copy }) {
+    setTimeout(() => {
+        document.querySelectorAll(".segment-text").forEach((el) => {
+            st += el.textContent + " ";
+        });
+        copy(st);
+    }, 3000);
+}
